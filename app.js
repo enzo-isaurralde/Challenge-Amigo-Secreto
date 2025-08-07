a2d3ca579fa2101e2bbfc605ea8f73c6c49edb50
enzo-isaurralde <enzoarielisaurralde@gmail.com>
2025-08-07 11:05:09 -0300

    Se agregaron funcion agregarNombre

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+
+function agregarNombre(){
+    let nombre = document.getElementById("amigo").value;
+    if (nombre.length === 0) {
+        alert("No se ingreso ningun nombre");
+        return;
+        
+    }
+
+nombres.push(nombre);
+console.log(nombres);
+document.getElementById("amigo").value = "";
+}
+
+
+
+
+
