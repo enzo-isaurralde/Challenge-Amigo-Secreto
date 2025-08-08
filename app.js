754009c115bcbf9cd1ce69f5ea4083d59af73a37
enzo-isaurralde <enzoarielisaurralde@gmail.com>
2025-08-08 12:59:59 -0300

    Se agregaron dos funciones y modificacion en html

--- app.js
+++ app.js
@@ -13,7 +13,32 @@ console.log(nombres);
 document.getElementById("amigo").value = "";
 }
 
+function asignarNombre(listaAmigos,texto) {
+    let lista = document.querySelector(listaAmigos);
+    lista.innerHTML = ""
+    let elementoHTML = "";
+    for (nombre of nombres) {
+    elementoHTML += `<li>${nombre}</li>`;
+    }
+    lista.innerHTML = elementoHTML;
+}
+    
+console.log(nombre);
+
+  
 
 
+function mostrarNombre() {
+    if (nombres.length > 0) {
+console.log("nombres no está vacío");
+} else {
+console.log("nombres está ingresado correctamente");  }
+
+let indiceAletorio = Math.floor(Math.random()*nombres.length);
+let nombreAleatorio = nombres[indiceAletorio];
+console.log(nombreAleatorio);
+let nombreGanador = document.getElementById("resultado");
+nombreGanador.innerHTML = nombreAleatorio;
+}
 
 
